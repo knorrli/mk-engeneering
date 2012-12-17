10acb29cf8e8c246252b6d15735408ae7f9f997d
Simon Kiener <jugglinghobo@gmail.com>
2012-12-17 02:11:27 +0100

    initial commit, added gems for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MkEngeneering::Application.config.secret_token = '0c28726965fe913376b3eb6e6a2fdfc47791eba62db7c1d034a88cfe2db85411ac0714d3557806dea4601f9e9543798c20a3c58795733ad1fd102fd27264cc15'
